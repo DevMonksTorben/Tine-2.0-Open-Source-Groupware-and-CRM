3c3ca97c0076bc24dab6cbe5a5a71144679e7d81
Alexander Stintzing <alex@stintzing.net>
2012-01-04 03:44:08 +0100

    refactoring to new application interface

--- tine20/Tinebase/js/widgets/MainScreen.js
+++ tine20/Tinebase/js/widgets/MainScreen.js
@@ -119,7 +119,9 @@ Ext.extend(Tine.widgets.MainScreen, Ext.util.Observable, {
      */
     getNorthPanel: function(contentType) {
         contentType = contentType || this.getActiveContentType();
-        
+        Tine.log.err('ALEX',contentType);
+        Tine.log.err('ALEX',this.centerPanelClassNameSuffix);
+        Tine.log.err('ALEX', this[contentType + this.centerPanelClassNameSuffix]);
         if (! this[contentType + 'ActionToolbar']) {
             try {
                 this[contentType + 'ActionToolbar'] = this[contentType + this.centerPanelClassNameSuffix].getActionToolbar();
